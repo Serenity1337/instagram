a1ed4742c70bf298a3a229c852fca421aa119804
serenity <43002637+Serenity1337@users.noreply.github.com>
2021-01-04 23:45:20 +0200

    build

---
+++ frontend/build/precache-manifest.91492613a9939bd164ed0cb058b53392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2561816fab1a74cada4ef9cf4aa9d373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11a3abc9af787f896567",
+    "url": "/static/css/main.4338ca99.chunk.css"
+  },
+  {
+    "revision": "5783f4658f1790295050",
+    "url": "/static/js/2.a2c17527.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a2c17527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a3abc9af787f896567",
+    "url": "/static/js/main.11b3ecb2.chunk.js"
+  },
+  {
+    "revision": "5a04474ba3d65d4810d6",
+    "url": "/static/js/runtime-main.ff298723.js"
+  }
+]);
